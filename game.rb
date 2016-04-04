901af6ccdaa88efe37545dc7efd13b11328960d9
railsbeginner <bartosz.dyja@gmail.com>
2016-04-04 18:38:16 +0200

    Configure router for dynamic urls

--- game.rb
+++ game.rb
@@ -2,6 +2,6 @@ class Game
   attr_reader :status, :turn
   def initialize
     @turn = %w(shoot save).sample
-    @status = {result: 0, turn: @turn, id: self.__id__}
+    @status = { result: 0, turn: @turn, id: __id__ }
   end
 end
--- server.rb
+++ server.rb
@@ -3,24 +3,35 @@ require './game'
 
 class Server
   attr_reader :server
+  attr_accessor :games
 
   def initialize(ip, port)
     @ip = ip
     @port = port
     @server = TCPServer.open(@ip, @port)
+    @games = []
     @game = Game.new
-    @games = {}
   end
 
   def start
     loop do
       conn = server.accept
-      request = conn.gets.split[1]
-      case request
+      request = conn.gets
+      request_url ||= request.split[1]
+      case request_url
+      when "/"
+        conn.puts 'Welcome to the game'
+        conn.puts 'Hit /start endpoint to begin.'
       when '/start'
-        conn.puts 'start request'
-      when '/shoot'
-        conn.puts 'shoot'
+        @game = Game.new
+        @games << @game
+        conn.puts @game.status
+      when %r(show\/\d+)
+        id = request_url.sub('/show/', '')
+        conn.puts id
+      when '/show'
+        # conn.puts @games
+        conn.puts @games.map(&:status)
       else
         conn.puts 'I don\'t understand'
       end

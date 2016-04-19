3043898b1be179bad2f2bed47ebb2e3e1013466d
railsbeginner <bartosz.dyja@gmail.com>
2016-04-19 12:11:24 +0200

    Add ending game logic

--- game.rb
+++ game.rb
@@ -1,14 +1,16 @@
 require './penalty'
 
 class Game
-  attr_accessor :status, :result
+  attr_accessor :status, :result, :score
   def initialize
     turn = %w(shoot save).sample
     @result = { you: [], computer: [] }
-    @status = { result: result, turn: turn, round: 1, score: [0,0] }
+    @score = { you: 0, computer: 0 }
+    @status = { result: result, turn: turn, round: 1, score: @score, active: true }
   end
 
   def shoot(coor)
+    return status.to_json unless status[:active] == true
     return 'Wrong action' unless status[:turn] == 'shoot'
     result[:you] << Penalty.new(coor).shoot
     status[:turn] = 'save'
@@ -17,6 +19,7 @@ class Game
   end
 
   def save(coor)
+    return status.to_json unless status[:active] == true
     return 'Wrong action' unless status[:turn] == 'save'
     result[:computer] << Penalty.new(coor).save
     status[:turn] = 'shoot'
@@ -26,9 +29,20 @@ class Game
   private
 
   def update_result
-    status[:round] = result[:you].size
-    status[:score][0] = result[:you].reduce(0,:+)
-    status[:score][1] = result[:computer].reduce(0,:+)
+    status[:round] = (result[:you] && result[:computer]).size + 1
+    score[:you] = result[:you].reduce(0, :+)
+    score[:computer] = result[:computer].reduce(0, :+)
+    if score[:you] == 3 && score[:computer] == 0
+      status[:active] = false
+    elsif score[:you] == 0 && score[:computer] == 3
+      status[:active] = false
+    elsif score[:you] == 4 && score[:computer] == 2
+      status[:active] = false
+    elsif score[:you] <= 2 && score[:computer] == 4
+      status[:active] = false
+    elsif score[:you] != score[:computer] && status[:round]>5
+      status[:active] = false
+    end
     status.to_json
   end
 end
--- run_game.rb
+++ run_game.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 require './server'
 
 Server.new('localhost', '1234').start
